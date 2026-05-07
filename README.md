1c65345d1f09e77ac4817eb3dd57d960270eebdf
SolidLamp <138074663+SolidLamp@users.noreply.github.com>
2026-05-07 19:29:21 +0100

    [lmc] Update to use better practices

--- README.md
+++ README.md
@@ -2,8 +2,21 @@
 
 ## gcbmk
 Language: Python
+
 A basic bin-packing algorithm with a database of the GameCube's library and the number of blocks each game uses to save.
+
+Usage:
+```shell
+$ python3 ./main.py
+```
 
 ## lmc
 Language: C99
+
 An implementation of the Little Man Computer instruction set. Note: only the binary codes, no assembler.
+
+Usage:
+```shell
+$ gcc main.c lmc.c -o out
+$ ./out
+```
--- lmc/lmc.c
+++ lmc/lmc.c
@@ -4,163 +4,173 @@
 
 #include "lmc.h"
 
-void print_arr(void);
-static uint16_t get_next_instruction(void);
-static void decode(int16_t i);
-static void handle_opcode(uint8_t opcode);
-static void add(uint8_t operand);
-static void sub(uint8_t operand);
-static void store(uint8_t operand);
-static void load(uint8_t operand);
-static void branch(uint8_t operand);
-static void branch_if_zero(uint8_t operand);
-static void branch_if_positive(uint8_t operand);
-static void io(uint8_t operand);
-static void illegal(void);
+typedef struct {
+    uint16_t arr[99];
+    int16_t acc;
+    uint8_t pc;
+    uint8_t ir;
+    uint8_t ar;
+} Memory;
 
-static uint16_t arr[99] = {0};
-static int16_t acc = 0;
-static uint8_t pc = 0;
-static uint8_t ir = 0;
-static uint8_t ar = 0;
+static void print_arr(Memory* mem);
+static Memory memory_init(void);
+static uint16_t get_next_instruction(Memory* mem);
+static void decode(Memory* mem, int16_t i);
+static void handle_opcode(Memory* mem, uint8_t opcode);
+static void add(Memory* mem, uint8_t operand);
+static void sub(Memory* mem, uint8_t operand);
+static void store(Memory* mem, uint8_t operand);
+static void load(Memory* mem, uint8_t operand);
+static void branch(Memory* mem, uint8_t operand);
+static void branch_if_zero(Memory* mem, uint8_t operand);
+static void branch_if_positive(Memory* mem, uint8_t operand);
+static void io(Memory* mem, uint8_t operand);
+static void illegal(Memory* mem);
 
 void run_cpu(uint16_t instructions[], int input_len)
 {
+    Memory mem = memory_init();
     input_len = input_len > 99 ? 99 : input_len;
     for (int i = 0; i < input_len; i++) {
-        arr[i] = instructions[i];
+        mem.arr[i] = instructions[i];
     }
-    print_arr();
+    print_arr(&mem);
     printf("\n");
-    int arr_len = sizeof(arr)/sizeof(arr[0]);
+    int arr_len = sizeof(mem.arr) / sizeof(mem.arr[0]);
     for (int i = 0; i < arr_len; i++) {
-        decode(get_next_instruction());
+        decode(&mem, get_next_instruction(&mem));
         /* printf("Current instruction:  %d", pc); */
-        handle_opcode(ir);
+        handle_opcode(&mem, mem.ir);
     }
 }
 
-void print_arr(void) 
+static Memory memory_init(void)
 {
-    int arr_len = sizeof(arr)/sizeof(arr[0]);
+    Memory mem = { { 0 }, 0, 0, 0, 0 };
+    return mem;
+}
+
+static void print_arr(Memory* mem)
+{
+    int arr_len = sizeof(mem->arr) / sizeof(mem->arr[0]);
     for (int i = 0; i < arr_len; i++) {
-        printf("%d|", arr[i]);
+        printf("%d|", mem->arr[i]);
     }
 }
 
-static uint16_t get_next_instruction(void) 
+static uint16_t get_next_instruction(Memory* mem)
 {
-    return arr[pc++];
+    return mem->arr[mem->pc++];
 }
 
-static void decode(int16_t instruction)
+static void decode(Memory* mem, int16_t instruction)
 {
-    ir = instruction / 100;
-    ar = instruction - (ir * 100);
+    mem->ir = instruction / 100;
+    mem->ar = instruction - (mem->ir * 100);
 }
 
-static void handle_opcode(uint8_t opcode)
+static void handle_opcode(Memory* mem, uint8_t opcode)
 {
     switch (opcode) {
-        case 0:
-            printf("Halt.\n");
-            exit(0);
-            break;
-        case 1:
-            add(ar);
-            break;
-        case 2:
-            sub(ar);
-            break;
-        case 3:
-            store(ar);
-            break;
-        case 5:
-            load(ar);
-            break;
-        case 6:
-            branch(ar);
-            break;
-        case 7:
-            branch_if_zero(ar);
-            break;
-        case 8:
-            branch_if_positive(ar);
-            break;
-        case 9:
-            io(ar);
-            break;
-        default:
-            illegal();
-            break;
+    case 0:
+        printf("Halt.\n");
+        exit(0);
+        break;
+    case 1:
+        add(mem, mem->ar);
+        break;
+    case 2:
+        sub(mem, mem->ar);
+        break;
+    case 3:
+        store(mem, mem->ar);
+        break;
+    case 5:
+        load(mem, mem->ar);
+        break;
+    case 6:
+        branch(mem, mem->ar);
+        break;
+    case 7:
+        branch_if_zero(mem, mem->ar);
+        break;
+    case 8:
+        branch_if_positive(mem, mem->ar);
+        break;
+    case 9:
+        io(mem, mem->ar);
+        break;
+    default:
+        illegal(mem);
+        break;
     }
 }
 
-static void add(uint8_t operand)
+static void add(Memory* mem, uint8_t operand)
 {
-    uint16_t to_add = arr[operand];
+    uint16_t to_add = mem->arr[operand];
     printf("Add %d to accumulator\n", to_add);
-    acc += to_add;
+    mem->acc += to_add;
 }
 
-static void sub(uint8_t operand)
+static void sub(Memory* mem, uint8_t operand)
 {
-    uint16_t to_sub = arr[operand];
+    uint16_t to_sub = mem->arr[operand];
     printf("Subtract %d from accumulator\n", to_sub);
-    acc -= to_sub;
+    mem->acc -= to_sub;
 }
 
-static void store(uint8_t operand)
+static void store(Memory* mem, uint8_t operand)
 {
     printf("Store value of accumulator at %d\n", operand);
-    arr[operand] = acc;
+    mem->arr[operand] = mem->acc;
 }
 
-static void load(uint8_t operand)
+static void load(Memory* mem, uint8_t operand)
 {
     printf("Load value of %d to accumulator\n", operand);
-    acc = arr[operand];
+    mem->acc = mem->arr[operand];
 }
 
-static void branch(uint8_t operand)
+static void branch(Memory* mem, uint8_t operand)
 {
     printf("Branch to %d\n", operand);
-    pc = operand;
+    mem->pc = operand;
 }
 
-static void branch_if_zero(uint8_t operand)
+static void branch_if_zero(Memory* mem, uint8_t operand)
 {
-    if (acc == 0)
-        branch(operand);
+    if (mem->acc == 0)
+        branch(mem, operand);
 }
 
-static void branch_if_positive(uint8_t operand)
+static void branch_if_positive(Memory* mem, uint8_t operand)
 {
-    if (acc >= 0)
-        branch(operand);
+    if (mem->acc >= 0)
+        branch(mem, operand);
 }
 
-static void io(uint8_t operand)
+static void io(Memory* mem, uint8_t operand)
 {
     switch (operand) {
-        case 1:
-            printf("Get user input\n");
-            int input;
-            scanf("%d", &input);
-            acc = (uint16_t)input;
-            break;
-        case 2:
-            printf("Output value of accumulator\n");
-            printf("%d\n", acc);
-            break;
-        default:
-            illegal();
-            break;
+    case 1:
+        printf("Get user input\n");
+        int input;
+        scanf("%d", &input);
+        mem->acc = (uint16_t)input;
+        break;
+    case 2:
+        printf("Output value of accumulator\n");
+        printf("%d\n", mem->acc);
+        break;
+    default:
+        illegal(mem);
+        break;
     }
 }
 
-static void illegal(void)
+static void illegal(Memory* mem)
 {
-    printf("Illegal instruction encountered: %d", arr[pc]);
+    printf("Illegal instruction encountered: %d", mem->arr[mem->pc]);
     exit(1);
 }
